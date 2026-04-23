2315b65964447df4e6d12facae666bd3e4f6c61f
jjanadi <jjanadi@Ubuntu.myguest.virtualbox.org>
2026-04-23 14:56:02 -0700

    Updated aesdchar hash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = d5319ee70c6ce759ac71c8767053b462eb002f96
+AESD_ASSIGNMENTS_VERSION = 8c937eaecd061926753a8b70bdcc60af7f6b670a
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
